6062f9ae15837cfc8ecfc528e0ee56d41c0c1436
Amsterdan Vasconcelos <135554841+Amsterdanvasconcelos@users.noreply.github.com>
2023-10-02 05:10:27 -0300

    add scroll com javascript (font: inkasadev)

---
+++ scroll-js-01/script.js
@@ -0,0 +1,16 @@
+const scrollLinks = document.querySelectorAll('.js-link')
+
+scrollLinks.forEach(link => {
+    link.addEventListener('click', event => {
+        const id = link.getAttribute('href')
+        const element = document.querySelector(id)
+        const position = element.offsetTop - 69
+
+        window.scrollTo({
+            top: position,
+            behavior: "smooth"
+        })
+
+        event.preventDefault()
+    })
+})
